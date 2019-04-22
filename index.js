10ed0dc3a23aabf773cbbcbed39a735bb9877edf
agyeiyb <agyeiyb@gmail.com>
2019-04-22 12:42:31 -0400

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ $(function(){
 $(".shopping-list").on("click", ".shopping-item-toggle", function(event) {
   $(this).closest("li").find(".shopping-item").toggleClass("shopping-item__checked");
 });
-
+ 
 $(".shopping-list").on("click", ".shopping-item-delete", function(event) {
   $(this).closest("li").remove();
 });
